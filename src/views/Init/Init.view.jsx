1d20607d27aed52ee31cbf0e63dc1116cd3db267
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-05-19 07:26:58 +0000

    primer 'commit'

---
+++ src/views/Init/Init.view.jsx
@@ -0,0 +1,12 @@
+import './init.style.scss'
+
+const Init = () => {
+  // * return
+  return (
+    <section className='init view'>
+      <h1>Init</h1>
+    </section>
+  )
+}
+
+export default Init
